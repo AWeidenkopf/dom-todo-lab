b0ee4bbe02c8f9b160c360bbd1b74609c06ef399
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-08-30 16:13:53 -0400

    solve lab

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log("hello")
+console.dir("hi")
+const usersInput = document.getElementById("input");
+console.dir(usersInput);
+const subButton = document.getElementById("submit-button");
+console.dir(subButton);
+const list = document.getElementById("todo-list");
+console.dir(list)
+const resetButton = document.getElementById("reset")
+
+subButton.addEventListener("click", addItems)
+resetButton.addEventListener("click", resetButtonFn)
+list.addEventListener("click", cleanEl)
+
+  function addItems(e) {
+  if(usersInput.value){
+    const newItem = document.createElement("li");
+    newItem.textContent = usersInput.value;
+    list.appendChild(newItem);
+    usersInput.value = "";
+  }
+}
+
+// I can't use a arrow function unless I put the eventlistener after it's inicialization
+
+function resetButtonFn(e) {
+  list.innerHTML = "";
+  usersInput.value = "";
+
+}
+
+function cleanEl(e) {
+  const parentNode = e.target.parentNode
+  parentNode.removeChild(e.target)
+}
