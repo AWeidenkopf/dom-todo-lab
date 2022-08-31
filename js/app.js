a9b08958086f36f11fa157199a60d6ba7d44a8b4
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-08-31 19:29:31 -0400

    fix code

--- js/app.js
+++ js/app.js
@@ -7,39 +7,42 @@ const list = document.getElementById("todo-list");
 
 const resetButton = document.getElementById("reset");
 
-
 subButton.addEventListener("click", addItems);
-resetButton.addEventListener("click", resetButtonFn);
-list.addEventListener("dblclick", cleanEl);
-list.addEventListener("click", crossEl);
+resetButton.addEventListener("click", resetButtonFn)
 
 function addItems(e) {
   if (usersInput.value) {
     const newItem = document.createElement("li");
     newItem.textContent = usersInput.value;
     list.appendChild(newItem);
+    newItem.addEventListener("dblclick", cleanEl);
+    newItem.addEventListener("click", crossEl);
     usersInput.value = "";
   }
 }
-
-// I can't use a arrow function unless I put the eventlistener after it's inicialization
+//my event listeners for the new items need to be inside of the fn block where 
+//it creates it/ otherwise I wouldn't access it
+// I can't use a arrow function unless I put the eventlistener after it's initialization
 
 function resetButtonFn(e) {
   list.innerHTML = "";
   usersInput.value = "";
 
 }
-function crossEl(e){
-  const parentNode = e.target.parentNode
-  parentNode.style.setProperty("text-decoration", "line-through");
+// .split(' ').some(decor => decor === "line-through")) 
+// since the text-decoration can have more than one value
+// it would be interesting to try an approach where if the are more
+// the cliclink wouldn't set it to none completely
+
+function crossEl(e) {
+  if (e.target.style.getPropertyValue("text-decoration")){
+    e.target.style.setProperty("text-decoration", "none");  
+  } else {
+    e.target.style.setProperty("text-decoration", "line-through underline");
+  }
 }
 
 function cleanEl(e) {
   const parentNode = e.target.parentNode
-  parentNode.ondblclick = () => parentNode.removeChild(e.target) // I can't have them outside of the function, for it will mess with the add to the list
-  document.addEventListener("keydown", (keyboardEvent) => {
-    if (keyboardEvent.key === "Control") {
-      parentNode.dispatchEvent(new MouseEvent("dblclick"))
-    }
-  })
+  parentNode.removeChild(e.target)
 }
