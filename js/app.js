3554323d9ff7dd421f4788e3043346a6f2e84d4d
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-08-31 08:41:43 -0400

    add strikethrough onclick/ remove on dblclick

--- js/app.js
+++ js/app.js
@@ -7,9 +7,11 @@ const list = document.getElementById("todo-list");
 
 const resetButton = document.getElementById("reset");
 
+
 subButton.addEventListener("click", addItems);
 resetButton.addEventListener("click", resetButtonFn);
-list.addEventListener("click", cleanEl);
+list.addEventListener("dblclick", cleanEl);
+list.addEventListener("click", crossEl);
 
 function addItems(e) {
   if (usersInput.value) {
@@ -27,8 +29,17 @@ function resetButtonFn(e) {
   usersInput.value = "";
 
 }
+function crossEl(e){
+  const parentNode = e.target.parentNode
+  parentNode.style.setProperty("text-decoration", "line-through");
+}
 
 function cleanEl(e) {
   const parentNode = e.target.parentNode
-  parentNode.removeChild(e.target)
+  parentNode.ondblclick = () => parentNode.removeChild(e.target) // I can't have them outside of the function, for it will mess with the add to the list
+  document.addEventListener("keydown", (keyboardEvent) => {
+    if (keyboardEvent.key === "Control") {
+      parentNode.dispatchEvent(new MouseEvent("dblclick"))
+    }
+  })
 }
